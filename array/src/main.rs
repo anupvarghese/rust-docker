4b3da02c2c8488b5d7f678f167268d6fde264df6
Anoop Varghese <anupvarghese@gmail.com>
2017-04-27 20:12:55 +1000

    subscript

--- array/src/main.rs
+++ array/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
   let other_list = [5;10]; // initialize with 5 for 10
   println!("{:?}", other_list);
   println!("{:?}", list);
+  println!("{:?}", list.len());
+  println!("{:?}", list[10]); // panic here --> this is how segmentation faults are avoided
 }
